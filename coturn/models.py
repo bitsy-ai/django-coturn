c9eac4dff6714e954c7aeb5f52a04bfba2f569d4
Leigh Johnson <hi@leighjohnson.me>
2021-09-18 15:11:04 -0700

    regenerate initial migrations

--- coturn/models.py
+++ coturn/models.py
@@ -1,36 +1,36 @@
 # References: https://github.com/coturn/coturn/blob/master/turndb/schema.sql
 
 from django.db import models
+
+from django.contrib.auth.models import AbstractUser
+
 from .settings import coturn_settings
 
 
-class AdminUser(models.Model):
+class TurnAdmin(models.Model):
     """
         A coturn admin_user row is created automatically for superusers
     """
-    name = models.CharField(unique=True, max_length=32, blank=True, null=True)
+    name = models.CharField(unique=True, max_length=32)
     realm = models.CharField(max_length=127, blank=True, null=True)
-    password = models.CharField(max_length=127, blank=True, null=True)
-    django_user = models.OneToOneField(
-        coturn_settings.get_user_model_string(),
-        null=True,
-        on_delete=models.SET_NULL,
-        related_name="coturn_admin_user",
-    )
+    password = models.CharField(max_length=127)
+    django_user_id = models.BigIntegerField()
 
     class Meta:
         db_table = "admin_user"
-        models.UniqueConstraint(fields=["realm", "name"], name="unique_user_per_realm")
-
-
+        constraints = (
+            models.UniqueConstraint(fields=["realm", "name"], name="unique_admin_username_per_realm"),
+            models.UniqueConstraint(fields=["realm", "django_user_id"], name="unique_django_admin_per_realm")
+        )
 class AllowedPeerIp(models.Model):
     realm = models.CharField(max_length=127, blank=True, null=True)
     ip_range = models.CharField(max_length=256, blank=True, null=True)
 
     class Meta:
         db_table = "allowed_peer_ip"
-        unique_together = (("realm", "ip_range"),)
-
+        constraints = (
+            models.UniqueConstraint(fields=["realm", "ip_range"], name="unique_allowed_ip_range_realm"),
+        )
 
 class DeniedPeerIp(models.Model):
     realm = models.CharField(max_length=127, blank=True, null=True)
@@ -38,8 +38,9 @@ class DeniedPeerIp(models.Model):
 
     class Meta:
         db_table = "denied_peer_ip"
-        unique_together = (("realm", "ip_range"),)
-
+        constraints = (
+            models.UniqueConstraint(fields=["realm", "ip_range"], name="unique_denied_ip_range_realm"),
+        )
 
 class OauthKey(models.Model):
     kid = models.CharField(unique=True, max_length=128)
@@ -68,10 +69,9 @@ class TurnRealmOption(models.Model):
 
     class Meta:
         db_table = "turn_realm_option"
-        models.UniqueConstraint(fields=["realm", "opt"], name="unique_opt_per_realm")
-
-        unique_together = (("realm", "opt"),)
-
+        constraints = (
+            models.UniqueConstraint(fields=["realm", "opt"], name="unique_opt_per_realm"),
+        )
 
 class TurnSecret(models.Model):
     realm = models.CharField(max_length=127)
@@ -79,10 +79,11 @@ class TurnSecret(models.Model):
 
     class Meta:
         db_table = "turn_secret"
-        models.UniqueConstraint(
+        constraints = (
+            models.UniqueConstraint(
             fields=["realm", "value"], name="unique_value_per_realm"
+            ),
         )
-
 
 class TurnUser(models.Model):
     """
@@ -91,13 +92,11 @@ class TurnUser(models.Model):
     realm = models.CharField(max_length=127, blank=True, default="")
     name = models.CharField(max_length=512, blank=True, null=True)
     hmackey = models.CharField(max_length=128, blank=True, null=True)
-    django_user = models.OneToOneField(
-        coturn_settings.get_user_model_string(),
-        null=True,
-        on_delete=models.SET_NULL,
-        related_name="coturn_user",
-    )
+    django_user_id = models.BigIntegerField()
 
     class Meta:
         db_table = "turnusers_lt"
-        models.UniqueConstraint(fields=["realm", "name"], name="unique_user_per_realm")
+        constraints = (
+            models.UniqueConstraint(fields=["realm", "name"], name="unique_username_per_realm"),
+            models.UniqueConstraint(fields=["realm", "django_user_id"], name="unique_django_user_per_realm")
+        )
