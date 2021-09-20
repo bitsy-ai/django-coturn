f513a94de141c9ee74c858c674fe9f4babe7a6a5
Leigh Johnson <leigh@bitsy.ai>
2021-09-19 17:42:08 -0700

    Implement user post-save signals (#5)
    * connect mgmt cmd to services, hook up signals
    
    * test coturn_sync turn_secret
    
    * add tests for turn_admin and turn_user sync commands
    
    * lint

---
+++ django_coturn/apps.py
@@ -0,0 +1,11 @@
+from django.apps import AppConfig
+
+
+class DjangoCoturnConfig(AppConfig):
+    name = "django_coturn"
+
+    def ready(self):
+        try:
+            import django_coturn.signals  # noqa F401
+        except ImportError:
+            pass
--- django_coturn/management/commands/coturn_sync.py
+++ django_coturn/management/commands/coturn_sync.py
@@ -7,8 +7,12 @@ import hashlib
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand, CommandParser
 from django.conf import settings
-from django_coturn.models import TurnSecret, TurnAdmin, TurnUser
-
+from django_coturn.services import (
+    delete_turn_secrets,
+    create_turn_secret,
+    get_or_update_turn_admin,
+    get_or_update_turn_user,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -32,52 +36,18 @@ class Command(BaseCommand):
 
     def handle_turn_user(self, *args, **kwargs):
         for user in User.objects.all():
-            try:
-                u = TurnUser.objects.get(name=user.email)
-                print(f"Skipping user with id={user.id} (already exists)")
-            except User.DoesNotExist:
-                password = User.objects.make_random_password()
-                hmackey = hmac.new(
-                    settings.COTURN_SECRET_KEY.encode("utf-8"),
-                    password.encode("utf-8"),
-                    hashlib.sha1,
-                )
-                hmackey.update(settings.COTURN_REALM.encode("utf-8"))
-                u = TurnUser.objects.create(
-                    user=user.email,
-                    realm=settings.COTURN_REALM,
-                    django_user_id=user.id,
-                    hmackey=hmackey.hexdigest(),
-                )
-                print(f"Created turnsusers_lt {u}")
+            turn_user = get_or_update_turn_user(user)
+            self.stdout.write(f"Created {turn_user} from {user}")
 
     def handle_turn_secret(self, *args, **kwargs):
-        realm = settings.COTURN_REALM
-        secret = settings.COTURN_SECRET_KEY
-        for s in TurnSecret.objects.using("coturn").all():
-            print(f"Deleting {s}")
-            s.delete(using="coturn")
-        new_secret = TurnSecret(realm=realm, value=secret)
-        new_secret.save(using="coturn")
-        print(f"Created {new_secret} from COTURN_SECRET_KEY")
+        delete_turn_secrets()
+        new_secret = create_turn_secret()
+        self.stdout.write(f"Created {new_secret} from COTURN_SECRET_KEY")
 
     def handle_turn_admin(self, *args, **kwargs):
+        for user in User.objects.filter(is_superuser=True).all():
+            turn_admin = get_or_update_turn_admin(user)
+            self.stdout.write(f"Created {turn_admin} from {user}")
 
-        for superuser in User.objects.filter(superuser=True).all():
-            try:
-                a = TurnAdmin.objects.get(name=superuser.email)
-                print(f"Skipping turn_admin creation for {superuser.email}")
-                if a.django_user_id is None:
-                    a.django_user_id = superuser.id
-                    a.save()
-                    print(
-                        f"Updated TURN admin_user {a} django_user_id={a.django_user_id}"
-                    )
-            except TurnAdmin.DoesNotExist:
-                a = TurnAdmin.objects.create(
-                    name=superuser.email,
-                    django_user_id=superuser.id,
-                    password="CHANGEME",
-                )
-
-                print(f"Created admin_user {a}")
+    def handle(self, subcommand: Callable, *args, **kwargs):
+        return subcommand(*args, **kwargs)
